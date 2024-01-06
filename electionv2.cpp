22fe2194789ce759e9fff177ca75649787fcae25
Hritik Kumar <2201020734@cgu-odisha.ac.in>
2024-01-06 21:53:14 +0530

    added function for declaring the winner but has a error in that function

--- electionv2.cpp
+++ electionv2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
-bool CheckForValidVoterID(int temp,int numOfVoters){
-    if (temp>numOfVoters){
-        cout<<"invalid Voter ID"<<endl;
+bool CheckForValidVoterID(int temp, int numOfVoters)
+{
+    if (temp > numOfVoters)
+    {
+        cout << "invalid Voter ID" << endl;
         return true;
     }
     return false;
@@ -11,8 +13,22 @@ bool CheckForValidVoterID(int temp,int numOfVoters){
 
 void printResults(int numOfCandidates, int **votesPerCandidate)
 {
+    for (int i = 1; i <= numOfCandidates; i++)
+        cout << "Candidate " << i << "'s vote count: " << votesPerCandidate[i][0] << "\n";
+}
+
+int DeclareWinner(int **votesPerCandidate, int numOfCandidates)
+{
+    int candidateId = 0, maxVote = votesPerCandidate[0][0];
     for (int i = 0; i < numOfCandidates; i++)
-        cout << "Candidate " << i << "'s vote count: " << votesPerCandidate[i + 1][0] << "\n";
+    {
+        if (votesPerCandidate[i+1][0] > maxVote)
+        {
+            maxVote = votesPerCandidate[i+1][0];
+            candidateId = i+1;
+        }
+    }
+    return candidateId;
 }
 
 // boolean return value, checks if voter has already voted
@@ -46,7 +62,6 @@ int main()
     int **votesPerCandidate = new int *[numOfCandidates + 1];
     for (int i = 0; i < numOfCandidates; i++)
         votesPerCandidate[i + 1] = new int[1];
-    
 
     int *alreadyVotedUsers = new int[numOfVoters]; // Array to track voted IDs
 
@@ -59,9 +74,10 @@ int main()
 
         cout << "Enter your voter ID: ";
         cin >> temp;
-        if (CheckForValidVoterID(temp,numOfVoters) == true)
+        // checking for the voter id entered lyes within the total no of voters
+        if (CheckForValidVoterID(temp, numOfVoters) == true)
             continue;
-        
+
         // if voter has already voted then skip current iteartion
         if (checkIfAlreadyVoted(numOfVoters, temp, alreadyVotedUsers) == true)
             continue;
@@ -72,7 +88,7 @@ int main()
         // dispaying all candidates with cID
         for (int i = 1; i <= numOfCandidates; i++)
             cout << "Press '" << i << "' for candidate " << i << "\n";
-        
+
         // choosing candidate to vote
         cin >> currentChosenVote;
         // printing which voter voted whom
@@ -83,16 +99,19 @@ int main()
         j++;
     }
 
-    // showing result
+    // showing result or votes received by each candidate
     printResults(numOfCandidates, votesPerCandidate);
+
+    // Declaration of winner
+    int winnerCandidate = DeclareWinner(votesPerCandidate, numOfCandidates);
+    cout << "The winner is:" << winnerCandidate;
 
     // Free dynamically allocated memory
     delete[] voters;
     delete[] alreadyVotedUsers;
     for (int i = 0; i < numOfCandidates; i++)
         delete[] votesPerCandidate[i];
-    
-    delete[] votesPerCandidate;
 
+    delete[] votesPerCandidate;
     return 0;
 }
